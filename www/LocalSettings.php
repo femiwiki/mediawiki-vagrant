60a8ff819ceb95fe9af24280f9b34a2f524850f4
suyooktang <suyook.tang@gmail.com>
2017-02-26 17:26:24 +0900

    Allow iframe

--- www/LocalSettings.php
+++ www/LocalSettings.php
@@ -102,6 +102,7 @@ require_once "$IP/extensions/HTMLTags/HTMLTags.php";
 $wgHTMLTagsAttributes['a'] = array( 'href', 'class', 'itemprop' );
 $wgHTMLTagsAttributes['link'] = array('href', 'itemprop');
 $wgHTMLTagsAttributes['meta'] = array('content', 'itemprop');
+$wgHTMLTagsAttributes['iframe'] = array('href', 'class');
 
 ## OpenGraphMeta
 require_once( "$IP/extensions/OpenGraphMeta/OpenGraphMeta.php" );
